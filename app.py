faa3418b88c1af75aa35b14d3447a904a7ee88f4
TyPage <typage89@gmail.com>
2022-12-02 14:05:09 -0600

    Setup flask, pip

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
-print("---------------------")
-print("the Farts?")
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
