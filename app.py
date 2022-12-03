92518400fa0612b64047b69f3c37095b010a0830
TyPage <typage89@gmail.com>
2022-12-03 16:01:29 -0600

    setup search, query data from CT API and display

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template, request
+from urllib.request import urlopen
+import json
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "GET":
+        return render_template("index.html")
+    if request.method == "POST":
+        condition = request.form.get("condition")
+        search_url = "https://ClinicalTrials.gov/api/query/study_fields?fmt=JSON&expr=%s&fields=NCTId,Condition,BriefTitle" % (condition)
+        response=urlopen(search_url)
+        data_json = json.loads(response.read())
+        return render_template("index.html", x = data_json)
